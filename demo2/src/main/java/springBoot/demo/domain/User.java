cf03f8c99cfacd4e26e6f0bcc08eea3f8e869750
=fus <fushuai_f@126.com>
2018-06-19 13:52:39 +0800

    init

---
+++ demo2/src/main/java/springBoot/demo/domain/User.java
@@ -0,0 +1,26 @@
+package springBoot.demo.domain;
+
+public class User {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private long id;
+    private String name;
+
+
+}
